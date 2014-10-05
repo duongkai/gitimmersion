3a26cc2c0dc22460932b1e69bc3d931f8f8ddec0
duongkai <phamtungduong@gmail.com>
2014-10-06 00:29:09 +0700

    add loooooppp

--- test.rb
+++ test.rb
@@ -11,3 +11,7 @@ b = (1..10).map {|x| x * x}
 square_sum = 0
 b.each {|x| square_sum += x}
 puts "Square of sum = #{square_sum}"
+
+square_sum = 0
+(1..10).map {|x| x * x}.each {|x| square_sum += x}
+puts "New square sum = #{square_sum}"
